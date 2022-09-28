be474aa9fc57977ade1f01e2d31d0d2c0872462c
catlover6 <wodbs0634@naver.com>
2022-09-28 18:51:27 +0900

    hw2

--- 195299 명재윤/195299 명재윤/195299 명재윤.cpp
+++ 195299 명재윤/195299 명재윤/195299 명재윤.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-	int a, b;
-	cout<<"en"
+	int n;
+	cout << "1~100사이의 숫자를 입력해주세요" << endl;
+	cin >> n;
+
+	if (1 <= n <= 100)
+	{
+		for (int i = 1; i <= 100; i++)
+		{
+			if (i % n == 0)
+				cout << i << " ";
+		}
+	}
+	return 0;
 }
