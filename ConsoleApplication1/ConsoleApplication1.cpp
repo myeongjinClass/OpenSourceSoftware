6c37282fe3b4dcc48bdff5c6c93d7553b5f21a50
wemul <2315_kim@naver.com>
2022-10-04 09:55:19 +0900

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 0;
+	int a = 0;
+	cout << "정수 입력 : ";
+	cin >> a;
+	for (i = 0; i < 100; i++)
+	{
+		if ((i + 1) % a == 0)
+			cout << i + 1;
+		cout << "\n";
+	}
+	return 0;
+}
