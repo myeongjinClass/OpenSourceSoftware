1655134708811ba119eaf662b650710343c33f0d
pttung15tt <114390691+pttung15tt@users.noreply.github.com>
2022-10-04 17:52:31 +0900

    Add files via upload

---
+++ 214237PHUNG_THANH-TUNG.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+ int n;   
+ cout<< "n를 입력하세요!";
+ cin>>n;
+  
+  for(int i=1; i<=100;i++)
+    if(i%n==0)
+    cout<<i<<" ";
+return 0;
+}
