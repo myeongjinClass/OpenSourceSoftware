f4b21f58240a53bea272b97535dd019b941cdd3c
HyeongJunJoinGo <june1873@naver.com>
2022-10-04 20:50:20 +0900

    HW2 update

---
+++ 195857 조형준/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, i;
+    cin >> N;
+    for (i = 1; i <= 100; i++){
+        if (i % N == 0) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
